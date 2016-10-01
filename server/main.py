01a8fdf7586f39bdac148cde8c5526762deb2f7e
Alex Klibisz <aklibisz@gmail.com>
2016-10-01 12:10:58 -0400

    got the static root working

--- server/main.py
+++ server/main.py
@@ -1,9 +1,24 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, send_from_directory
 
+# Setup Flask app.
 app = Flask(__name__)
+app.debug = True
+app._static_folder = '../client'
+
+# Routes
+@app.route('/')
+def root():
+  return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+  return app.send_static_file(path)
 
 @app.route('/video', methods=['POST'])
 def json_handler():
 	data = json.loads(request.data)
 	return json.dumps(data)
+
+if __name__ == '__main__':
+  app.run()
