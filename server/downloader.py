a8beaa2d9d9d4928095399e0cc06edf383c2b16b
Alex Klibisz <aklibisz@gmail.com>
2016-10-01 11:50:09 -0400

    commit

--- server/downloader.py
+++ server/downloader.py
@@ -1,1 +1,28 @@
-# Functions for downloading a video go here
+#!/usr/bin/env python
+from pytube import YouTube
+import math
+import cv2
+
+#this function downloads a video by url
+#and uses openCV to return its image array
+def extract_files(url):
+        #set filename to id
+        yt = YouTube(url)
+        id = url.split("v=")[-1] if len(url.split("v=")) == 2 else url.rsplit('/', 1)[-1]
+        yt.set_filename(id)
+        #download video of certain quality
+        video = yt.get('mp4', '360p')
+        video.download('.')
+
+        frames = []
+        cap = cv2.VideoCapture(id+".mp4")
+        frameRate = cap.get(5) #frame rate
+        while(cap.isOpened()):
+                frameId = cap.get(1) #current frame number
+                ret, frame = cap.read()
+                if (ret != True):
+                        break
+                if (frameId % math.floor(frameRate) == 0):
+                        frames.append(frame)
+        cap.release()
+        return frames
