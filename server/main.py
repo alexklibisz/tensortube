65e3e42dd699b771bde9f1c73a88b09ff1227827
Doug <douglasaaser@gmail.com>
2016-10-01 11:41:13 -0400

    changed name

--- server/main.py
+++ server/main.py
@@ -1,1 +1,9 @@
-# Entry-point for the server
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/video', methods=['POST'])
+def json_handler():
+	data = json.loads(request.data)
+	return json.dumps(data)
--- server/server.py
+++
@@ -1,9 +0,0 @@
-import json
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/video', methods=['POST'])
-def json_handler():
-	data = json.loads(request.data)
-	return json.dumps(data)
