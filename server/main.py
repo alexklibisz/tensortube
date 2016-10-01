a8ec81ab7a2374a0ac03bb4925410c029ad1b4da
Grant Bruer <grbr9001@gmail.com>
2016-10-01 15:34:09 -0400

    Everything works. Trust me.

--- server/main.py
+++ server/main.py
@@ -15,6 +15,7 @@ app._static_folder = '../client'
 # Setup classifier.
 # Creates classification network and node ID --> English string lookup.
 node_lookup = classifier.NodeLookup()
+videoFolder = './server/videos'
 
 # Routes
 @app.route('/')
@@ -30,13 +31,24 @@ def json_handler():
 
     classifier.create_graph()
     reqData = json.loads(request.data)
+
+    filename = videoFolder + "/" + downloader.get_youtube_id(reqData['url']) + ".json"
+    print (filename) #filename = "./server/videos/YbcxU1IK7s4.json"
+    try:
+        cached = open(filename, 'r')
+        print("Found %s in JSON cache" % (filename))
+        return cached.read()
+    except:
+        print("Didn't find %s in JSON cache" % (filename))
+        pass
+
     print (reqData['url'])
     frames = downloader.extract_files(reqData['url'])
-    print ("%d frames" % (len(frames)))
+    print ("%d frame%s" % (len(frames), 's' if len(frames) > 1 else ''))
+
     resData = {"labels" : {}}
+    # Add important frames to resData
     for t, f in enumerate(frames):
-        if t > 5:
-            break
         img_str = cv2.imencode('.jpg', f)[1].tostring()
         top_predictions, all_predictions = classifier.get_top_predictions_jpg_data(img_str, 1)
 
@@ -50,20 +62,37 @@ def json_handler():
                     resData["labels"][human_string] = {"times" : []}
                 resData["labels"][human_string]["times"].append(t)
             print('frame %d %s (score = %.5f)' % (t, human_string, score))
-
+    
     # Response data should be formatted like this.
     # resData = {
     #     "labels": {
     #         "cat" : {
-    #             "times": [40,50,60,70]
+    #             "times": [40,50,60,70],
+    #             "scores" : [0.8, 0.3, 0.4, 0.9]
     #         },
     #         "dog" : {
     #             "times": [120,130,140]
+    #             "scores" : [0.8, 0.3, 0.4]
     #         }
     #     }
     # }
+
+
+    # Remove any consecutive label times
+    for label, obj in resData["labels"].items():
+        start = 1
+        times = obj["times"]
+        for i in range(1, len(times), 1):
+            if times[i] - times[i-1] > 1:
+                times[start] = times[i]
+                start = start + 1
+        obj["times"] = times[:start]
+
     print(resData)
-    return(json.dumps(resData))
+    resJson = json.dumps(resData)
+    cached = open(filename, 'w')
+    cached.write(resJson)
+    return resJson
 
 
 
